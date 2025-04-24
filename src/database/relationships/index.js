26cf5be402b108814e474c31b784914bc142274b
bergad37 <bergad37@gmail.com>
2025-04-24 17:57:05 +0200

    set up sequlize

---
+++ src/database/relationships/index.js
@@ -0,0 +1,4 @@
+import DB from '../index';
+const associate = () => {};
+
+export default associate;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import http from 'http';
 import app from './app';
+import connectDB from './services/connectDB';
 
 const port = process.env.PORT || 700;
 const server = http.createServer(app);
 
 const startServer = () => {
+  connectDB();
   server.listen(PORT, () => {
     console.log(`Server listening to port ${PORT} `);
   });
